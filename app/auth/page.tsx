a8b8bea924f465578c0bd763f9a93404bbb89104
Surya <suryakant.subudhi@outlook.com>
2023-08-26 11:35:17 +0530

    💢Added animated inputs in auth

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,3 +1,4 @@
+import Input from "@/components/input"
 import Image from "next/image"
 
 const Auth = () => {
@@ -22,6 +23,29 @@ const Auth = () => {
                 <nav className="px-12 py-5">
                     <Image src={"/images/logo.png"} alt="logo" width={90} height={60}/>
                 </nav>
+                <div className="flex justify-center">
+                    <div
+                        className="
+                            bg-black
+                            bg-opacity-70
+                            px-16
+                            py-16
+                            self-center
+                            mt-2
+                            lg:w-2/5
+                            lg:max-w-md
+                            rounded-md
+                            w-full
+                        "
+                    >
+                        <h2 className="text-white text-4xl mb-2 font-semibold">
+                            Sign In
+                        </h2>
+                        <div className="flex flex-col gap-4">
+                            <Input />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
