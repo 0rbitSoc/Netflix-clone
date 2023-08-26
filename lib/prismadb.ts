200fe2d0302b6d0656efa388f52f39f88b9c8822
Surya <suryakant.subudhi@outlook.com>
2023-08-26 20:05:03 +0530

    📋Bpr

---
+++ lib/prismadb.ts
@@ -0,0 +1,17 @@
+// Prevents NextJS hot reloading to cause any problem while initualizing prisma
+
+
+import { PrismaClient } from  "@prisma/client";
+
+declare global {
+    var prisma: PrismaClient | undefined;
+}
+
+const prismadb = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+
+export default prismadb;
+
+// Resolves the problem of multiple prisma instances
+// while nextjs hot reloading!
