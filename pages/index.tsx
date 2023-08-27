95976fea87b7502221b2cd733281e3a783e1114c
Surya <suryakant.subudhi@outlook.com>
2023-08-27 17:08:45 +0530

    🔐Protected routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,31 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from "react";
+import { getSession, signOut } from "next-auth/react";
+import { NextPageContext } from "next";
 
-const inter = Inter({ subsets: ['latin'] })
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context);
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: "/auth",
+                permanent: false,
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
+}
+
 
 export default function Home() {
   return (
-    <h1 className='text-white'>Netflix clone</h1>
+    <>
+        <h1 className='text-white text-4xl'>Netflix clone</h1>
+        <button className="h-10 w-50 bg-white" onClick={() => signOut()}>Log Out</button>
+
+    </>
   )
 }
