e77da838e604a2895ad85135bb103acfe2c70d75
Surya <suryakant.subudhi@outlook.com>
2023-09-02 13:55:05 +0530

    🐻Added info to movie card

--- components/moviecard.tsx
+++ components/moviecard.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
-import { BsFillPlayFill } from "react-icons/bs"
+import { BsFillPlayFill } from "react-icons/bs";
+import { BiChevronDown } from "react-icons/bi";
 import FavouriteButton from "./favouritebutton";
 import { useRouter } from "next/navigation";
+import useInfoModal from "@/hooks/useInfoModal";
 
 interface MovieCardProps {
     data: Record<string, any>;
@@ -11,6 +13,7 @@ interface MovieCardProps {
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
 
     const router = useRouter();
+    const { openModal } = useInfoModal();
 
     return (
         <div className="group bg-zinc-900 col-span relative h-[12vw]">
@@ -80,6 +83,9 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                             <BsFillPlayFill />
                         </div>
                         <FavouriteButton movieId={data?.id}/>
+                        <div onClick={() => openModal(data?.id)} className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+                            <BiChevronDown size={30} className="text-white group-hover/item:text-neutral-300" />
+                        </div>
                     </div>
                     <p className="text-green-400 font-semibold mt-4">
                         New <span className="text-white">
